e835cc3214a2753fc0832138e9712c6868604bc2
jorgemarpa-exalearn <jorgemarpa@berkeley.edu>
2020-09-30 23:05:02 -0700

    adding new vae scripts with phy

---
+++ src/vae_training_phy.py
@@ -0,0 +1,346 @@
+import numpy as np
+import datetime
+import torch
+import torch.nn as nn
+from torch import autograd
+from src.utils import plot_recon_wall, plot_latent_space
+from src.training_callbacks import EarlyStopping
+
+
+class Trainer():
+    """
+    A training class for VAE model. Incorporate batch-training/testing loop,
+    training callbacks, lerning rate scheduler, W&B logger with metrics reports and
+    reconstruction plots.
+    ...
+    Attributes
+    ----------
+    train_loss  : dict
+        dictionary with training metrics
+    test_loss   : dict
+         dictionary with testing metrics
+    num_steps   : int
+        number of training steps
+    print_every : int
+        report metric values every N training steps
+    beta        : str
+        value of beta hyperparameter that weight the KLD term in the loss function
+    wb          : object
+        weight and biases logger
+    Methods
+    -------
+    _loss(self, x, xhat, mu, logvar, train=True, ep=0)
+        calculate loss metrics and add them to logger
+    _train_epoch(self, data_loader, epoch)
+        do model training in a given epoch
+    _test_epoch(self, test_loader, epoch)
+        do model testing in a given epoch
+    _report_train(self, i)
+        report training metrics to W&B logger and standard terminal
+    _report_test(self, ep)
+        report test metrics to W&B logger and standard terminal
+    train(self, train_loader, test_loader, epochs, data_ex,
+          machine='local', save=True, early_stop=False)
+        complete epoch training/validation/report loop
+    """
+    def __init__(self, model, optimizer, batch_size, wandb,
+                 scheduler=None, print_every=50, beta='step',
+                 device='cpu'):
+        """
+        Parameters
+        ----------
+        model       : pytorch module
+            pytorch VAE model
+        optimizer   : pytorch optimizer
+            pytorch optimizer object
+        batch_size  : int
+            size of batch for training loop
+        wandb       : object
+            weight and biases logger
+        scheduler   : object
+            pytorch learning rate schduler, default is None
+        print_every : int
+            step interval for report printing
+        device      : str
+            device where model will run (cpu, gpu)
+        """
+        self.device = device
+        self.model = model
+        if torch.cuda.device_count() > 1 and True:
+            print("Let's use", torch.cuda.device_count(), "GPUs!")
+            self.model = nn.DataParallel(self.model)
+        self.model.to(self.device)
+        print('Is model in cuda? ', next(self.model.parameters()).is_cuda)
+        self.opt = optimizer
+        self.sch = scheduler
+        self.batch_size = batch_size
+        self.train_loss = {'Loss': [], 'BCE': [], 'KLD': []}
+        self.test_loss = {'Loss': [], 'BCE': [], 'KLD': []}
+        self.num_steps = 0
+        self.print_every = print_every
+        self.mse_loss = nn.MSELoss(reduction='mean')
+        self.bce_loss = nn.BCELoss(reduction='mean')
+        #self.kld_loss = nn.KLDivLoss(reduction='mean')
+        self.wb = wandb
+        self.beta = beta
+        
+        
+    def _beta_scheduler(self, epoch, beta0=0., step=15, gamma=0.2):
+        """Scheduler for beta value, the sheduler is a step function that
+        increases beta value after "step" number of epochs by a factor "gamma"
+        Parameters
+        ----------
+        epoch : int
+            epoch value
+        beta0 : float
+            starting beta value
+        step  : int
+            epoch step for update
+        gamma : float
+            linear factor of step scheduler
+        Returns
+        -------
+        beta
+            beta value
+        """
+        if self.beta == 'step':
+            return beta0 + gamma * (epoch // step)
+        else:
+            return float(self.beta)
+
+
+    def _loss(self, x, xhat, mu, logvar, train=True, ep=0):
+        """Evaluates loss function and add reports to the logger.
+        
+        Parameters
+        ----------
+        x      : tensor
+            tensor of real values
+        xhat   : tensor
+            tensor of predicted values
+        train  : bool
+            wheather is training step or not
+        ep     : int
+            epoch value of training loop
+        Returns
+        -------
+        loss
+            loss value
+        """
+        rec = self.bce_loss(xhat, x)
+        kld_l = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        loss = rec + self._beta_scheduler(ep) * kld_l
+
+        if train:
+            self.train_loss['BCE'].append(rec.item())
+            self.train_loss['KLD'].append(kld_l.item())
+            self.train_loss['Loss'].append(loss.item())
+        else:
+            self.test_loss['BCE'].append(rec.item())
+            self.test_loss['KLD'].append(kld_l.item())
+            self.test_loss['Loss'].append(loss.item())
+
+        return loss
+
+    def _train_epoch(self, data_loader, epoch):
+        """Training loop for a given epoch. Triningo goes over
+        batches, images and latent space plots are logged to
+        W&B
+        Parameters
+        ----------
+        data_loader : pytorch object
+            data loader object with training items
+        epoch       : int
+            epoch number
+        Returns
+        -------
+        """
+        # switch model to training mode
+        self.model.train()
+        # iterate over len(data)/batch_size
+        mu_all = []
+        xhat_plot, x_plot = [], []
+        for i, (img, phy) in enumerate(data_loader):
+            self.num_steps += 1
+            self.opt.zero_grad()
+            img = img.to(self.device)
+            phy = phy.to(self.device)
+
+            with autograd.detect_anomaly():
+                xhat, z, mu, logvar = self.model(img, phy=phy)
+                # calculate loss value
+                loss = self._loss(img, xhat, mu, logvar,
+                                  train=True, ep=epoch)
+                # calculate the gradients
+                loss.backward()
+                # perform optimization step accordig to the gradients
+                self.opt.step()
+
+            self._report_train(i)
+            # aux variables for latter plots
+            mu_all.append(mu.data.cpu().numpy())
+            if i == len(data_loader) - 2:
+                xhat_plot = xhat.data.cpu().numpy()
+                x_plot = img.data.cpu().numpy()
+            if i == 5: continue
+
+        mu_all = np.concatenate(mu_all)
+        mu_all = mu_all[np.random.choice(mu_all.shape[0], 5000,
+                                         replace=False), :]
+
+        # plot reconstructed images ever 2 epochs
+        if epoch % 2 == 0:
+            wall = plot_recon_wall(xhat_plot, x_plot, epoch=epoch)
+            self.wb.log({'Train_Recon':  self.wb.Image(wall)},
+                        step=self.num_steps)
+
+        if epoch % 2 == 0:
+            latent_plot = plot_latent_space(mu_all, y=None)
+            self.wb.log({'Latent_space': self.wb.Image(latent_plot)},
+                        step=self.num_steps)
+
+    def _test_epoch(self, test_loader, epoch):
+        """Testing loop for a given epoch. Triningo goes over
+        batches, images and latent space plots are logged to
+        W&B logger
+        Parameters
+        ----------
+        data_loader : pytorch object
+            data loader object with training items
+        epoch       : int
+            epoch number
+        Returns
+        -------
+        """
+        # swich model to evaluation mode, this make it deterministic
+        self.model.eval()
+        with torch.no_grad():
+            xhat_plot, x_plot = [], []
+
+            for i, (img, phy) in enumerate(test_loader):
+                # send data to current device
+                img = img.to(self.device)
+                phy = phy.to(self.device)
+                xhat, z, mu, logvar = self.model(img, phy=phy)
+                # calculate loss value
+                loss = self._loss(img, xhat, mu, logvar,
+                                  train=False, ep=epoch)
+
+                # aux variables for plots
+                if i == len(test_loader) - 2:
+                    xhat_plot = xhat.data.cpu().numpy()
+                    x_plot = img.data.cpu().numpy()
+                if i == 5: continue
+
+        self._report_test(epoch)
+
+        # plot reconstructed images ever 2 epochs
+        if epoch % 2 == 0:
+            wall = plot_recon_wall(xhat_plot, x_plot, epoch=epoch)
+            self.wb.log({'Test_Recon':  self.wb.Image(wall)},
+                        step=self.num_steps)
+
+        return loss
+
+    def train(self, train_loader, test_loader, epochs,
+              save=True, early_stop=False):
+        """Full training loop over all epochs. Model is saved after
+        training is finished.
+        Parameters
+        ----------
+        train_loader : pytorch object
+            data loader object with training items
+        test_loader : pytorch object
+            data loader object with training items
+        epoch       : int
+            epoch number
+        save        : bool
+            wheather to save or not final model
+        early_stop  : bool
+            whaether to use early stop callback which stops training
+            when validation loss converges
+        Returns
+        -------
+        """
+
+        # hold samples, real and generated, for initial plotting
+        if early_stop:
+            early_stopping = EarlyStopping(patience=10, min_delta=.1,
+                                           verbose=True)
+
+        # train for n number of epochs
+        time_start = datetime.datetime.now()
+        for epoch in range(1, epochs + 1):
+            e_time = datetime.datetime.now()
+            print('##'*20)
+            print("\nEpoch {}".format(epoch))
+
+            # train and validate
+            self._train_epoch(train_loader, epoch)
+            val_loss = self._test_epoch(test_loader, epoch)
+
+            # update learning rate according to cheduler
+            if self.sch is not None:
+                self.wb.log({'LR': self.opt.param_groups[0]['lr']},
+                            step=self.num_steps)
+                if 'ReduceLROnPlateau' == self.sch.__class__.__name__:
+                    self.sch.step(val_loss)
+                else:
+                    self.sch.step(epoch)
+
+            # report elapsed time per epoch and total run tume
+            epoch_time = datetime.datetime.now() - e_time
+            elap_time = datetime.datetime.now() - time_start
+            print('Time per epoch: ', epoch_time.seconds, ' s')
+            print('Elapsed time  : %.2f m' % (elap_time.seconds/60))
+            print('##'*20)
+
+            # early stopping
+            if early_stop:
+                early_stopping(val_loss.cpu())
+                if early_stopping.early_stop:
+                    print("Early stopping")
+                    break
+
+        if save:
+            torch.save(self.model.state_dict(), '%s/model.pt' %
+                       (self.wb.run.dir))
+
+    def _report_train(self, i):
+        """Report training metrics to logger and standard output.
+        Parameters
+        ----------
+        i : int
+            training step
+        Returns
+        -------
+        """
+        # ------------------------ Reports ---------------------------- #
+        # print scalars to std output and save scalars/hist to W&B
+        if i % self.print_every == 0:
+            print("Training iteration %i, global step %i" %
+                  (i + 1, self.num_steps))
+            print("Loss: %.6f" % (self.train_loss['Loss'][-1]))
+
+            self.wb.log({'Train_Loss': self.train_loss['Loss'][-1]},
+                        step=self.num_steps)
+            print("__"*20)
+
+    def _report_test(self, ep):
+        """Report testing metrics to logger and standard output.
+        Parameters
+        ----------
+        i : int
+            testing step
+        Returns
+        -------
+        """
+        # ------------------------ Reports ---------------------------- #
+        # print scalars to std output and save scalars/hist to W&B
+        print('*** TEST LOSS ***')
+        print("Epoch %i, global step %i" % (ep, self.num_steps))
+        print("Loss: %.6f" % (self.test_loss['Loss'][-1]))
+
+        self.wb.log({'Test_Loss': self.test_loss['Loss'][-1]},
+                    step=self.num_steps)
+        print("__"*20)
---
+++ vae_main_phy.py
@@ -0,0 +1,195 @@
+"""VAE main training script
+This script allows the user to train an AE model using Protoplanetary disk
+images loaded with the 'dataset.py' class, VAE model located in 'ae_model.py' class,
+and the trainig loop coded in 'ae_training.py'. 
+The script also uses Weight & Biases framework to log metrics, model hyperparameters, configuration parameters, and training figures.
+This file contains the following
+functions:
+    * run_code - runs the main code
+For help, run:
+    python ae_main.py --help
+"""
+
+import sys
+import argparse
+import torch
+import torch.optim as optim
+import numpy as np
+from src.dataset_large import ProtoPlanetaryDisks
+from src.vae_model_phy import *
+from src.vae_training_phy import Trainer
+from src.utils import count_parameters
+import wandb
+
+torch.autograd.set_detect_anomaly(True)
+
+# set random seed
+rnd_seed = 13
+np.random.seed(rnd_seed)
+torch.manual_seed(rnd_seed)
+torch.cuda.manual_seed_all(rnd_seed)
+#os.environ['PYTHONHASHSEED'] = str(rnd_seed)
+
+# program flags
+parser = argparse.ArgumentParser(description='Variational AutoEncoder')
+parser.add_argument('--dry-run', dest='dry_run', action='store_true',
+                    default=False,
+                    help='Load data and initialize models [False]')
+parser.add_argument('--machine', dest='machine', type=str, default='local',
+                    help='were to is running (local, colab, [exalearn])')
+
+parser.add_argument('--data', dest='data', type=str, default='PPD',
+                    help='data used for training (MNIST, [PPD])')
+
+parser.add_argument('--lr', dest='lr', type=float, default=1e-4,
+                    help='learning rate [1e-4]')
+parser.add_argument('--lr-sch', dest='lr_sch', type=str, default=None,
+                    help='learning rate shceduler '+
+                    '([None], step, exp, cosine, plateau)')
+parser.add_argument('--beta', dest='beta', type=str, default='1',
+                    help='beta factor for latent KL div ([1],step)')
+parser.add_argument('--batch-size', dest='batch_size', type=int, default=32,
+                    help='batch size [128]')
+parser.add_argument('--num-epochs', dest='num_epochs', type=int, default=100,
+                    help='total number of training epochs [100]')
+parser.add_argument('--early-stop', dest='early_stop', action='store_true',
+                    default=False, help='Early stoping')
+
+parser.add_argument('--cond', dest='cond', type=str, default='T',
+                    help='physics conditioned AE ([F],T)')
+parser.add_argument('--feed-phy', dest='feed_phy', type=str, default='F',
+                    help='feed physics to decoder ([F],T)')
+parser.add_argument('--latent-dim', dest='latent_dim', type=int, default=32,
+                    help='dimension of latent space [32]')
+parser.add_argument('--dropout', dest='dropout', type=float, default=0.2,
+                    help='dropout for all layers [0.2]')
+parser.add_argument('--kernel-size', dest='kernel_size', type=int, default=3,
+                    help='2D conv kernel size, encoder [3]')
+parser.add_argument('--conv-blocks', dest='conv_blocks', type=int, default=5,
+                    help='conv+actfx+pool blocks [5]')
+parser.add_argument('--model-name', dest='model_name', type=str, 
+                    default='Linear_AE', help='name of model')
+
+parser.add_argument('--comment', dest='comment', type=str, default='',
+                    help='extra comments')
+args = parser.parse_args()
+
+# Initialize W&B project and save user defined flags
+wandb.init(entity='deep_ppd', project="PPD-VAE", tags=['VAE'])
+wandb.config.update(args)
+wandb.config.rnd_seed = rnd_seed
+
+
+# run main program
+def run_code():
+    # asses which device will be used, CPY or GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if device.type == 'cuda':
+        torch.cuda.empty_cache()
+    # Load Data #
+    if args.data == 'PPD':
+        dataset = ProtoPlanetaryDisks(machine=args.machine, transform=True,
+                                      img_norm=True)
+    elif args.data == 'MNIST':
+        dataset = MNIST(args.machine)
+    else:
+        print('Error: Wrong dataset (MNIST, Proto Planetary Disk)...')
+        raise
+
+    if len(dataset) == 0:
+        print('No items in training set...')
+        print('Exiting!')
+        sys.exit()
+    print('Dataset size: ', len(dataset))
+    
+    # data loaders for training and testing
+    train_loader, val_loader = dataset.get_dataloader(batch_size=args.batch_size,
+                                                       shuffle=True,
+                                                       val_split=.2,
+                                                       random_seed=rnd_seed)
+
+    if args.data == 'PPD' and args.cond == 'T':
+        wandb.config.physics_dim = len(dataset.par_names)
+    else:
+        wandb.config.physics_dim = 0
+        args.feed_phy = 'F'
+    wandb.config.update(args, allow_val_change=True)
+
+    print('Physic dimension: ', wandb.config.physics_dim)
+
+    # Define AE model, Ops, and Train #
+    # To used other AE models change the following line,
+    # different types of AE models are stored in src/ae_model.py
+    if args.model_name == 'ConvLinTrans_AE':
+        model = ConvLinTrans_AE(latent_dim=args.latent_dim,
+                                img_dim=dataset.img_dim,
+                                in_ch=dataset.img_channels,
+                                kernel=args.kernel_size,
+                                n_conv_blocks=args.conv_blocks,
+                                phy_dim=wandb.config.physics_dim,
+                                feed_phy=args.feed_phy)
+        
+    else:
+        print('Wrong Model Name.')
+        print('Please select model: ConvLinTrans_AE')
+        sys.exit()
+    
+    # log model architecture and gradients to wandb
+    wandb.watch(model, log='gradients')
+
+    wandb.config.n_train_params = count_parameters(model)
+    print('Summary:')
+    print(model)
+    print('Num of trainable params: ', wandb.config.n_train_params)
+    print('\n')
+
+    # Initialize optimizers
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-6)
+
+    # Learning Rate scheduler
+    if args.lr_sch == 'step':
+        scheduler = optim.lr_scheduler.StepLR(optimizer,
+                                              step_size=25,
+                                              gamma=0.5)
+    elif args.lr_sch == 'exp':
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer,
+                                                     gamma=0.985)
+    elif args.lr_sch == 'cos':
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                         T_max=50,
+                                                         eta_min=1e-5)
+    elif args.lr_sch == 'plateau':
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                         mode='min',
+                                                         factor=.5,
+                                                         verbose=True)
+    else:
+        scheduler = None
+
+    print('Optimizer    :', optimizer)
+    print('LR Scheduler :', scheduler.__class__.__name__)
+
+    print('########################################')
+    print('########  Running in %4s  #########' % (device))
+    print('########################################')
+
+    # initialize trainer
+    trainer = Trainer(model, optimizer, args.batch_size, wandb,
+                      scheduler=scheduler, print_every=500,
+                      device=device, beta=args.beta)
+
+    if args.dry_run:
+        print('******** DRY RUN ******** ')
+        return
+
+    # run training/testing iterations
+    trainer.train(train_loader, val_loader, args.num_epochs,
+                  save=True, early_stop=args.early_stop)
+
+
+if __name__ == "__main__":
+    print('Running in: ', args.machine, '\n')
+    for key, value in vars(args).items():
+        print('%15s\t: %s' % (key, value))
+
+    run_code()
