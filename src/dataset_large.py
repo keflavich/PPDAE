fixing image normalization

src/dataset_large.py
@@ -101,7 +101,7 @@ class ProtoPlanetaryDisks(Dataset):
         return a dataloader object for trainning and testing
     """
     def __init__(self, machine='exalearn', transform=True, par_norm=False,
-                 subset=''):
+                 subset='', image_norm='global'):
         """
         Parameters
         ----------
@@ -128,8 +128,8 @@ class ProtoPlanetaryDisks(Dataset):
         self.par_train = np.load('%s/param_arr_gridandfiller123%s_train_all.npy' %
                                  (ppd_path, subset))
         
-        self.imgs_paths = sorted(glob.glob('%s/img_array_gridandfiller123_norm%s_train_*.npy' %
-                                           (ppd_path, subset)))
+        self.imgs_paths = sorted(glob.glob('%s/img_array_gridandfiller123_%snorm%s_train_*.npy' %
+                                           (ppd_path, image_norm, subset)))
         self.imgs_memmaps = [np.load(path, mmap_mode='r') for path in self.imgs_paths]
         self.start_indices = [0] * len(self.imgs_paths)
         self.data_count = 0
@@ -142,8 +142,8 @@ class ProtoPlanetaryDisks(Dataset):
                            'Rin', 'sd_exp', 'a_max', 'inc']
         self.par_test = np.load('%s/param_arr_gridandfiller123%s_test.npy' % 
                                 (ppd_path, subset))
-        self.imgs_test = np.load('%s/img_array_gridandfiller123_norm%s_test.npy' % 
-                                 (ppd_path, subset))
+        self.imgs_test = np.load('%s/img_array_gridandfiller123_%snorm%s_test.npy' % 
+                                 (ppd_path, image_norm, subset))
 
         self.img_dim = self.imgs_test[0].shape[-1]
         self.img_channels = self.imgs_test[0].shape[0]