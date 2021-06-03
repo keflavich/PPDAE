c5e3c347826274d52b0768aa908ccdb679fb9c95
EWolfe5 <77813324+EWolfe5@users.noreply.github.com>
2021-06-02 17:03:46 -0700

    Fixed bug at instantiation

--- src/ae_model_phy.py
+++ src/ae_model_phy.py
@@ -1060,7 +1060,7 @@ class Forward_AE(nn.Module):
         in_ch      : int
             number of channels in input/output image
         """
-        super(ConvLinTrans_AE, self).__init__()
+        super(Forward_AE, self).__init__()
         self.img_width = self.img_height = img_dim
         self.img_size = self.img_width * self.img_height
         self.in_ch = in_ch
