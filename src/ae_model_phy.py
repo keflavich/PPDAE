4fd12833a3301ef4dea2824cfa89e70a5bd802e6
EWolfe5 <77813324+EWolfe5@users.noreply.github.com>
2021-06-14 14:59:37 -0700

    fixed typo (stride)

--- src/ae_model_phy.py
+++ src/ae_model_phy.py
@@ -102,7 +102,7 @@ class Forward_AE(nn.Module):
 
 class Dev_Forward_AE(nn.Module):
 
-    def __init__(self, img_dim=28, dropout=.2, in_ch=1, phy_dim=8, Stride=2, kernel_size=4):
+    def __init__(self, img_dim=28, dropout=.2, in_ch=1, phy_dim=8, stride=2, kernel_size=4):
         """
         Parameters
         ----------
@@ -148,7 +148,7 @@ class Dev_Forward_AE(nn.Module):
         
         #convolutional layers
         self.dec_transconv = nn.Sequential(
-            nn.ConvTranspose2d(16, 16,  kernel_size, stride=Stride, bias=False,
+            nn.ConvTranspose2d(16, 16,  kernel_size, stride=stride, bias=False,
                                output_padding=1, padding=0),
             nn.Conv2d(16, 16, 4, bias=False),
             nn.BatchNorm2d(16, momentum=0.005),
@@ -157,7 +157,7 @@ class Dev_Forward_AE(nn.Module):
             nn.Conv2d(16, 8, kernel_size, bias=False),
             nn.BatchNorm2d(8, momentum=0.005),
             nn.ReLU(),
-            nn.ConvTranspose2d(8, 8, kernel_size, stride=Stride, bias=False,
+            nn.ConvTranspose2d(8, 8, kernel_size, stride=stride, bias=False,
                                output_padding=1, padding=0),
             
             nn.Conv2d(8, 8, kernel_size, bias=False),
@@ -168,7 +168,7 @@ class Dev_Forward_AE(nn.Module):
             nn.BatchNorm2d(4, momentum=0.005),
             nn.ReLU(),
             ###output layer
-            nn.ConvTranspose2d(4, 4, kernel_size, stride=Stride, bias=False,
+            nn.ConvTranspose2d(4, 4, kernel_size, stride=stride, bias=False,
                                output_padding=1, padding=0),
             nn.Conv2d(4, in_ch, 7),
             nn.Sigmoid()
